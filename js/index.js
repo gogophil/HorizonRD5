728bac577a09b569a1c6cad1c4a475cdfacc9fd2
unknown <alexisdiamond@hotmail.ca>
2015-10-25 17:31:20 -0400

    Création du fichier des dossiers et fichiers de base

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* 
+    Created on : 2015-10
+    Author     : Alexis Diamond & Philippe Messier
+*/
+
+function loadMenus() {
+    var element = [{pageTitle: "index"}
+    
+    ];
+    
+    for(i = 0; i < element.length; i++) {
+        
+    }
+}
